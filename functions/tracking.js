fb0282e443ed7a0c2480438e0028b7a00b575579
timosieber <timo.sieber@trendingmedia.ch>
2024-10-24 14:23:51 +0200

    Add files via upload

---
+++ functions/tracking.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+
+exports.handler = async (event, context) => {
+  const clientId = event.queryStringParameters.client_id;
+
+  if (!clientId) {
+    return {
+      statusCode: 400,
+      body: 'client_id fehlt'
+    };
+  }
+
+  // URL für Google Analytics (mit deiner Measurement-ID und API-Schlüssel)
+  const postUrl = `https://www.google-analytics.com/mp/collect?measurement_id=G-EVX82XD4NH&api_secret=FVa3v2tYRWei_RLhQLFAmw`;
+
+  const postData = {
+    "client_id": clientId,
+    "events": [
+      {
+        "name": "email_open",
+        "params": {
+          "engagement": "open"
+        }
+      }
+    ]
+  };
+
+  try {
+    // Anfrage an Google Analytics senden
+    await axios.post(postUrl, postData);
+
+    // Tracking erfolgreich -> Sende das transparente Pixel-Bild zurück
+    const pixelPath = path.join(__dirname, '../images/transparent_pixel.png');
+    const pixelBuffer = fs.readFileSync(pixelPath);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'image/png'
+      },
+      body: pixelBuffer.toString('base64'),
+      isBase64Encoded: true
+    };
+  } catch (error) {
+    console.error('Fehler beim Tracking:', error);
+    return {
+      statusCode: 500,
+      body: 'Fehler beim Tracking'
+    };
+  }
+};
